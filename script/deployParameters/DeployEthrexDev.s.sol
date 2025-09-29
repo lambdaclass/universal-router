62beaa8cb9627d929a52f05c79e80d257fbfd7df
Leandro Serra <leandro.serra@lambdaclass.com>
2025-09-29 15:16:17 -0300

    feat(universal-router): support ethrex

---
+++ script/deployParameters/DeployEthrexDev.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import {DeployUniversalRouter} from '../DeployUniversalRouter.s.sol';
+import {RouterParameters} from 'contracts/base/RouterImmutables.sol';
+
+contract DeployEthrexDev is DeployUniversalRouter {
+    function setUp() public override {
+        params = RouterParameters({
+            permit2: 0x000000000022D473030F116dDEE9F6B43aC78BA3,
+            weth9: 0xeC7ed8038B76DbcB8F78b189EFf9D7C7373A45BE,
+            seaportV1_5: UNSUPPORTED_PROTOCOL,
+            seaportV1_4: UNSUPPORTED_PROTOCOL,
+            openseaConduit: UNSUPPORTED_PROTOCOL,
+            nftxZap: UNSUPPORTED_PROTOCOL,
+            x2y2: UNSUPPORTED_PROTOCOL,
+            foundation: UNSUPPORTED_PROTOCOL,
+            sudoswap: UNSUPPORTED_PROTOCOL,
+            elementMarket: UNSUPPORTED_PROTOCOL,
+            nft20Zap: UNSUPPORTED_PROTOCOL,
+            cryptopunks: UNSUPPORTED_PROTOCOL,
+            looksRareV2: UNSUPPORTED_PROTOCOL,
+            routerRewardsDistributor: UNSUPPORTED_PROTOCOL,
+            looksRareRewardsDistributor: UNSUPPORTED_PROTOCOL,
+            looksRareToken: UNSUPPORTED_PROTOCOL,
+            v2Factory: UNSUPPORTED_PROTOCOL,
+            v3Factory: 0xdEb1481F062db511358b2497ad1C2eB23DB76225,
+            pairInitCodeHash: BYTES32_ZERO,
+            poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54
+        });
+
+        unsupported = 0x0000000000000000000000000000000000000000;
+    }
+}
